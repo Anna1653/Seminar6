4e42bccb3e9f024224e46e1dc89ec07f20c42cab
Anna <anuta1653@mail.ru>
2023-06-29 23:33:21 +0300

    Zadacha41

---
+++ Zadazha41/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.WriteLine("Введите числа: ");
+
+string[] numsStr = Console.ReadLine().Split(" ");
+
+int m = 0;
+for (int i = 0; i < numsStr.Length; i++)
+{
+    if (Convert.ToInt32(numsStr[i]) > 0) 
+    {
+        m++;
+    }
+}
+Console.WriteLine($"Вы ввели {m} чисел (числа) больше нуля");
